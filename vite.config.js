53fa2963427390760896fa965fca81a954358e40
impeccable548 <igheleraro2@gmail.com>
2026-02-13 19:45:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Change output from 'dist' to 'build'
-  },
+  // We removed the outDir: 'build' line so it defaults to 'dist'
 })
