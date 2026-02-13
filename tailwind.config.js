c24fedac86e8f030c9f86afe82dea9187f46ddc9
impeccable548 <igheleraro2@gmail.com>
2026-02-13 18:41:17 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        romantic: ['"Dancing Script"', 'cursive'],
+        modern: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
